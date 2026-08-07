898c26358ccdffc9842b509597437ee662a990cd
alessandro-stella <alessandro.stella2004@gmail.com>
2026-08-07 18:54:43 +0200

    Avoid reactivation on suspend

--- hosts/gpu/nvidia.nix
+++ hosts/gpu/nvidia.nix
@@ -7,9 +7,17 @@
     modesetting.enable = true;
     nvidiaSettings = true;
 
+    # Allow suspention
+    powerManagement.enable = true;
+    powerManagement.kernelSuspendNotifier = false;
+
     # Use only on newer gpus
     open = true;
 
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  boot.extraModprobeConfig = ''
+    options nvidia NVreg_PreserveVideoMemoryAllocations=1
+  '';
 }
